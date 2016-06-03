f2152099fa3d9b83a178a8a541ffc19f17056551
krenuds <donthefringe@gmail.com>
2016-06-03 13:00:26 -0400

    added callbacks to HUI example.

--- examples/AdafrHUItWifi/AdafrHUItWifi.ino
+++ examples/AdafrHUItWifi/AdafrHUItWifi.ino
@@ -1,17 +1,13 @@
 // These need to be included when using standard Ethernet
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <WiFiUdp.h>
+#include <WiFiUDP.h>
 
 #include "AppleMidi.h"
 
-#define RED_LED 14
-#define GREEN_LED 13
-#define BLUE_LED 12
-
 
 char ssid[] = "WNSOM"; //  your network SSID (name)
-char pass[] = "1westnewburyschoolofmusic";    // your network password (use for WPA, or use as key for WEP)
+char pass[] = "password";    // your network password (use for WPA, or use as key for WEP)
 
 //char ssid[] = "NETGEAR53"; //  your network SSID (name)
 //char pass[] = "roundpond693";    // your network password (use for WPA, or use as key for WEP)
@@ -27,38 +23,43 @@ APPLEMIDI_CREATE_INSTANCE(WiFiUDP, AppleMIDI); // see definition in AppleMidi_De
 void setup()
 {
   Serial.begin(115200);
-
-  Serial.print("\nGetting IP address...");
-
-WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pass);
-WiFi.mode(WIFI_STA);
-
+  Serial.print("\nGetting IP address...");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  pinMode(RED_LED, OUTPUT);
-  pinMode(BLUE_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
 
 
   // Serial communications and wait for port to open:
-  KillLEDs ();
+
   Serial.println();
   Serial.print("IP address is ");
   Serial.println(WiFi.localIP());
 
-  AppleMIDI.begin("Feather");
+  AppleMIDI.begin("DonFeather");
 
   AppleMIDI.OnConnected(OnAppleMidiConnected);
   AppleMIDI.OnDisconnected(OnAppleMidiDisconnected);
-
-  AppleMIDI.OnReceiveNoteOn(OnAppleMidiNoteOn);
-  AppleMIDI.OnReceiveNoteOff(OnAppleMidiNoteOff);
-  AppleMIDI.OnReceiveControlChange(OnMidiControlChange);
-  AppleMIDI.OnReceiveProgramChange(OnMidiProgramChange);
-
+  
+  AppleMIDI.OnReceiveNoteOn(OnMidiReceiveNoteOn);
+  AppleMIDI.OnReceiveNoteOff(OnMidiReceiveNoteOff);
+  AppleMIDI.OnReceiveControlChange(OnMidiReceiveControlChange);
+  AppleMIDI.OnReceiveAfterTouchPoly(OnMidiReceiveAfterTouchPoly);
+  AppleMIDI.OnReceiveProgramChange(OnMidiReceiveProgramChange);
+  AppleMIDI.OnReceiveAfterTouchChannel(OnMidiReceiveAfterTouchChannel);
+  AppleMIDI.OnReceivePitchBend(OnMidiReceivePitchBend);
+  AppleMIDI.OnReceiveSystemExclusive(OnMidiReceiveSystemExclusive);
+  AppleMIDI.OnReceiveTimeCodeQuarterFrame(OnMidiReceiveTimeCodeQuarterFrame);
+  AppleMIDI.OnReceiveSongPosition(OnMidiReceiveSongPosition);
+  AppleMIDI.OnReceiveSongSelect(OnMidiReceiveSongSelect);
+  AppleMIDI.OnReceiveTuneRequest(OnMidiReceiveTuneRequest);
+  AppleMIDI.OnReceiveClock(OnMidiReceiveClock);
+  AppleMIDI.OnReceiveStart(OnMidiReceiveStart);
+  AppleMIDI.OnReceiveContinue(OnMidiReceiveContinue);
+  AppleMIDI.OnReceiveStop(OnMidiReceiveStop);
+  AppleMIDI.OnReceiveActiveSensing(OnMidiReceiveActiveSensing);
+  AppleMIDI.OnReceiveSystemReset(OnMidiReceiveSystemReset);
 
 
 
@@ -89,63 +90,42 @@ void OnAppleMidiDisconnected(uint32_t ssrc) {
 //
 // -----------------------------------------------------------------------------
 
-void OnAppleMidiNoteOn(byte channel, byte note, byte velocity) {
-  Serial.print("Incoming NoteOn from channel:");
-  Serial.print(channel);
-  Serial.print(" note:");
-  Serial.print(note);
-  Serial.print(" velocity:");
-  Serial.print(velocity);
-  Serial.println();
+void OnMidiReceiveNoteOff(byte channel, byte note, byte velocity) {
+  if (channel == 1 && note == 0 && velocity == 0) {
+    AppleMIDI.noteOn(0, 127, 1);
+    AppleMIDI.noteOff(0, 127, 1);
+  }
 }
 
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
-void OnAppleMidiNoteOff(byte channel, byte note, byte velocity) {
+void OnMidiReceiveNoteOn(byte channel, byte note, byte velocity) {
   if (channel == 1 && note == 0 && velocity == 0) {
     AppleMIDI.noteOn(0, 127, 1);
     AppleMIDI.noteOff(0, 127, 1);
   }
-  else {
-    Serial.print("Incoming NoteOff from channel:");
-    Serial.print(channel);
-    Serial.print(" note:");
-    Serial.print(note);
-    Serial.print(" velocity:");
-    Serial.print(velocity);
-    Serial.println();
-  }
-
 }
 
-void OnMidiControlChange(byte channel, byte note, byte velocity) {
-  if (channel = 1) {
-    if (note = 44) {
-      if (velocity == 67)
-        digitalWrite (RED_LED, HIGH);
+  void OnMidiReceiveTimeCodeQuarterFrame(byte data){
+    Serial.printf("OnMidiReceiveTimeCodeQuarterFrame: %02x\n", data);
     }
-  }
-//  Serial.print("Incoming CC from channel:");
-//  Serial.print(channel);
-//  Serial.print(" note:");
-//  Serial.print(note);
-//  Serial.print(" velocity:");
-//  Serial.print(velocity);
-//  Serial.println();
-}
-
-void OnMidiProgramChange(byte channel, byte note) {
-  //Serial.print("Incoming Prog Change from channel:");
-  //Serial.print(channel);
-  //Serial.print(" note:");
-  //Serial.print(note);
-  //Serial.println();
-}
 
 
-void KillLEDs() {
-  digitalWrite(RED_LED, LOW);
-  digitalWrite(BLUE_LED, LOW);
-  digitalWrite(GREEN_LED, LOW);
-}
+  
+  void OnMidiReceiveControlChange(byte channel, byte note, byte pressure){/*Serial.println("OnMidiReceiveControlChange");*/}  
+  void OnMidiReceiveAfterTouchPoly(byte channel, byte note, byte pressure){Serial.println("OnMidiReceiveAfterTouchPoly");}
+  void OnMidiReceiveProgramChange(byte channel, byte number){Serial.println("OnMidiReceiveProgramChange");}
+  void OnMidiReceiveAfterTouchChannel(byte channel, byte pressure){Serial.println("OnMidiReceiveAfterTouchChannel");}
+  void OnMidiReceivePitchBend(byte channel, int bend){Serial.println("OnMidiReceivePitchBend");}
+  void OnMidiReceiveSystemExclusive(byte * array, byte size){Serial.println("OnMidiReceiveSystemExclusive");}
+
+  void OnMidiReceiveSongPosition(unsigned int beats){Serial.println("OnMidiReceiveSongPosition");}
+  void OnMidiReceiveSongSelect(byte songnumber){Serial.println("OnMidiReceiveSongSelect");}
+  void OnMidiReceiveTuneRequest(){Serial.println("OnMidiReceiveTuneRequest");}
+  void OnMidiReceiveClock(){Serial.println("OnMidiReceiveClock");}
+  void OnMidiReceiveStart(){Serial.println("OnMidiReceiveStart");}
+  void OnMidiReceiveContinue(){Serial.println("OnMidiReceiveContinue");}
+  void OnMidiReceiveStop(){Serial.println("OnMidiReceiveStop");}
+  void OnMidiReceiveActiveSensing(){Serial.println("OnMidiReceiveActiveSensing");}
+  void OnMidiReceiveSystemReset(){Serial.println("OnMidiReceiveSystemReset");}
